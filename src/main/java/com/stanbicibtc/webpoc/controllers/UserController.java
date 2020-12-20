84548c7aa4d641016b0afef09885df39631a009b
ik umeh <ik.francis@yahoo.com>
2020-12-20 14:52:55 -0500

    update

--- src/main/java/com/stanbicibtc/webpoc/controllers/UserController.java
+++ src/main/java/com/stanbicibtc/webpoc/controllers/UserController.java
@@ -34,7 +34,7 @@ public class UserController {
     
     @GetMapping("/")
     public String index() {
-        return "/index";
+        return "index";
     }
     
     @GetMapping("/link_profile")
